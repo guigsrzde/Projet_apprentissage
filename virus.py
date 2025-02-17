a412895d2be1288e1f8d32e19d314bc349a462b0
Guillaume <rouzade.guillaume@gmail.com>
2025-02-17 18:47:22 +0100

    chgt coeffs

--- virus.py
+++ virus.py
@@ -70,9 +70,9 @@ class Virus:
         self.length_infection = self.infection_duration + sum([self.symptoms[name].duration_impact for name in self.symptoms.keys()])
         self.mortality_symptoms = sum([self.symptoms[name].mortality_impact for name in self.symptoms.keys()])
 
-        self.transmission_rate = 0.3*th((self.propagation)/3)
-        self.healing_rate = 1/(50*th((self.length_infection)/3))
-        self.mortality_rate = 0.1*th(self.mortality_symptoms/3)
+        self.transmission_rate = 0.3*th((self.propagation)/10)
+        self.healing_rate = 1/(50*th((self.length_infection)/10))
+        self.mortality_rate = 0.1*th(self.mortality_symptoms/10)
         return
 
 
