1d97913ae81e9291080aac6c1b8c688ecfb97b42
jeannesurgithub <161345804+jeannesurgithub@users.noreply.github.com>
2025-03-11 18:21:21 +0100

    plus de infected=true

--- royaume_uni/cities.py
+++ royaume_uni/cities.py
@@ -5,5 +5,5 @@ Edinburgh = [326, 305]
 Cardiff = [333, 552]
 Belfast = [244, 386]
 
-list_cities = [City(8700000, 435, 558, "London", 0, 50, 0.3, 0.1, infected=True), City(512000, 320, 299, "Edinburgh", 1, 50, 0.3, 0.1), 
+list_cities = [City(8700000, 435, 558, "London", 0, 50, 0.3, 0.1), City(512000, 320, 299, "Edinburgh", 1, 50, 0.3, 0.1), 
           City(372000, 330, 552, "Cardiff", 2, 50, 0.3, 0.1), City(348000, 241, 384, "Belfast", 3, 50, 0.3, 0.1)]
