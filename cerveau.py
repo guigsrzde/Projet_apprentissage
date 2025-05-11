c74cdfdc7e645aa7e7bc263d8e88ba0e2368a6e4
Guillaume <rouzade.guillaume@gmail.com>
2025-05-11 16:31:14 +0200

    création d'un agent qui sait rien faire (le code pour le faire jouer au jeu n'est pas encore fait)

---
+++ cerveau.py
@@ -0,0 +1,32 @@
+from gamedata import GameData
+from history import action
+
+class Brain:
+    def __init__(self, model):
+        self.model=model
+
+    def choose_action(self,state):
+        return self.model.predict_action(state)
+    
+    def learn(self, state, action, reward):
+        self.model.update(state, action, reward)
+
+class NoSkill:
+    def __init__self():
+        return
+
+    def predict_action(self, state:GameData):
+        while state.virus.mutation_points >= list(state.virus.symptoms.keys())[0].mutation_cost:
+            return action(list(state.virus.symptoms.keys())[0])  #list(state.virus.symptoms.keys())[0] corresponds to the name of the symptom
+        else:
+            return None
+    def update(self, state, action, reward):
+        return None
+
+
+
+try:
+    cerveau_basique
+except NameError:
+    cerveau_basique = Brain(NoSkill())
+
